2faf62fac80593f7236ca94c17312ecdfdc44f0f
Romina Tornello <rominautnmza@gmail.com>
2023-03-20 15:06:50 +0100

    init project

---
+++ src/main/java/com/rtornello/inditex/InditexApplication.java
@@ -0,0 +1,14 @@
+package com.rtornello.inditex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InditexApplication {
+
+	public static void main(String[] args) {
+		System.out.println("--------------------");
+		SpringApplication.run(InditexApplication.class, args);
+	}
+
+}
