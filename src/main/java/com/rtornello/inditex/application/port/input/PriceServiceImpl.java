6c29500bf51937a7e7001e1d36c226c96b4ade48
Romina Tornello <rominautnmza@gmail.com>
2023-03-28 15:53:12 +0200

    filter by prioridad

--- src/main/java/com/rtornello/inditex/application/port/input/PriceServiceImpl.java
+++ src/main/java/com/rtornello/inditex/application/port/input/PriceServiceImpl.java
@@ -4,15 +4,19 @@ package com.rtornello.inditex.application.port.input;
 import com.rtornello.inditex.application.interfaces.PriceService;
 import com.rtornello.inditex.application.port.output.PriceRepository;
 import com.rtornello.inditex.domain.model.Price;
+import com.rtornello.inditex.infraestructure.adapters.output.data.PriceData;
 import com.rtornello.inditex.infraestructure.adapters.output.mapper.PriceMapper;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
 import org.springframework.stereotype.Service;
-
 
 @Service
 public class PriceServiceImpl implements PriceService {
 
+    public static final String YYYY_MM_DD_HH_MM = "yyyy-MM-dd HH:mm";
     private final PriceRepository priceRepository;
 
     private final PriceMapper priceMapper;
@@ -23,9 +27,19 @@ public class PriceServiceImpl implements PriceService {
     }
 
     public Price getPrice(String dateTime, Integer productId, Integer brandId) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(YYYY_MM_DD_HH_MM);
         LocalDateTime dateTimeFormatter = LocalDateTime.parse(dateTime, formatter);
-        return  priceMapper.priceDataToPrice(priceRepository.findOneByStartDateLessThanEqualAndEndDateGreaterThanEqual(dateTimeFormatter, dateTimeFormatter));
+        List<PriceData> priceDataList = priceRepository.findOneByStartDateLessThanEqualAndEndDateGreaterThanEqual(dateTimeFormatter, dateTimeFormatter);
+
+        PriceData result = priceDataList.stream()
+            .max(Comparator.comparing(PriceData::getPriority))
+            .orElseThrow(NoSuchElementException::new);
+// TODO ADD EXCEPTION
+//        if (result == null) {
+//            throw new PriceNotFoundException("No Price found with these parameters");
+//        }
+
+        return  priceMapper.priceDataToPrice(result);
     }
 
 
--- src/main/java/com/rtornello/inditex/application/port/output/PriceRepository.java
+++ src/main/java/com/rtornello/inditex/application/port/output/PriceRepository.java
@@ -1,18 +1,17 @@
 package com.rtornello.inditex.application.port.output;
 
 import com.rtornello.inditex.infraestructure.adapters.output.data.PriceData;
-
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PriceRepository extends JpaRepository<PriceData, Serializable> {
 
-    PriceData findByProductIdAndBrandId(Integer product, Integer brandId);
 
-    PriceData findOneByStartDateLessThanEqualAndEndDateGreaterThanEqual(LocalDateTime endDate, LocalDateTime startDate);
+    List<PriceData> findOneByStartDateLessThanEqualAndEndDateGreaterThanEqual(LocalDateTime endDate, LocalDateTime startDate);
 
 
 }
